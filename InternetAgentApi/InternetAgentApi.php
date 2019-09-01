22589cf06f947143634134d13031caee49692c9d
Evgeniy Korepov <evgeniy.korepov@gmail.com>
2019-09-01 22:20:33 +0300

    Create InternetAgentApi.php

---
+++ InternetAgentApi/InternetAgentApi.php
@@ -0,0 +1,72 @@
+<?php                           
+
+@ini_set("display_errors", "1"); error_reporting(E_ALL);
+//@ini_set("display_errors", "0"); error_reporting(0);
+
+include_once("InternetAgentApiConfig.php");
+include_once("InternetAgentApiHelper.php");
+
+$RemoteIP = $_SERVER["REMOTE_ADDR"];
+
+MyLog($RemoteIP, $_REQUEST);
+
+$Request = GetRequest();
+if ($Request === false) {
+	echo GetAnswerError($Request);
+	return;
+}
+
+$_SESSION["Request"] = $Request;
+
+$ErrorMessage = "";
+
+switch ($Request["method"]) {
+
+	case "config" :
+		$Header = "Content-Type: application/json; charset=utf-8";
+		$Content = GetAnswerConfig($Request);
+		break;
+
+	case "news" :
+		$Header = "Content-Type: text/html; charset=utf-8";
+		include_once("InternetAgentApiContentNews.php");
+		$Content = GetPageNews($Request);
+		break;
+
+	case "info" :
+		$Header = "Content-Type: text/html; charset=utf-8";
+		include_once("InternetAgentApiContentInfo.php");
+		$Content = GetPageInfo($Request);
+		if ($Content === false)
+			$Content = GetAnswerErrorHTTP("Неизвестная ошибка, обратитесь в техподдержку.");
+		break;
+
+	case "services" :
+		$Header = "Content-Type: text/html; charset=utf-8";
+		include_once("InternetAgentApiContentServices.php");
+		$Content = GetPageServices($Request);
+		if ($Content === false)
+			$Content = GetAnswerErrorHTTP("Неизвестная ошибка, обратитесь в техподдержку.");
+		break;
+
+	case "send_notification" :
+		$Header = "Content-Type: text/html; charset=utf-8";
+		InitMysqli($mysqli);
+		$Content = SendNotification($Request);
+		break;
+
+	default : 
+		echo GetAnswerError($Request);
+		exit;
+}
+
+header($Header);
+
+if ($Content !== false) {
+	echo $Content;
+} else {
+	MyLog("Ansfer", $Content);
+	echo GetAnswerError($ErrorMessage);
+}
+
+
